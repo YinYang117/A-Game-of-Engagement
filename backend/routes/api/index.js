693d3749c01d239488233582bcda5010ad3e4b37
root <root@Kishi-Kitsune>
2024-05-28 21:26:54 -0600

    api route tested and cleanup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,13 +1,15 @@
+const e = require('express');
+
 const router = require('express').Router();
 
-// // Add a XSRF-TOKEN cookie return in json during development
-// // Currently no measures to stop it from working in production
-// router.get("/csrf/restore", (req, res) => {
-//     const csrfToken = req.csrfToken();
-//     res.cookie("XSRF-TOKEN", csrfToken);
-//     res.status(200).json({
-//         'XSRF-Token': csrfToken
-//     });
-// });
+// Add a XSRF-TOKEN cookie return in json
+router.get("/csrf/restore", (req, res) => {
+    const csrfToken = req.csrfToken();
+    res.cookie("XSRF-TOKEN", csrfToken);
+    res.status(200).json({
+        'XSRF-Token': csrfToken
+    });
+});
+
 
 module.exports = router;
