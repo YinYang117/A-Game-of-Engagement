ea0a9265d8736b4dd83663972a48497d6c34a0cc
YinYang117 <slcryan2010@gmail.com>
2025-05-16 15:53:55 -0600

    api index updates with notes, and validation error formatting setup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
+const { restoreUser } = require('../utilities/auth');
 
-// Add a XSRF-TOKEN cookie return in json
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+
+// If valid user in session:
+//  sets req.user to safeUser scope from db
+// Else sets req.user to null
+router.use(restoreUser);
+
+
+router.get("/", (_req, res, _next) => {
+    res.json("Welcome to the API");
+});
+
+
+// reset CSRF token, set the cookie, return XSRF-TOKEN cookie
 router.get("/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
     res.cookie("XSRF-TOKEN", csrfToken);
