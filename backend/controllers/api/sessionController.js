aa266d86cdd61118089e783e5be86f964e49b760
YinYang117 <slcryan2010@gmail.com>
2025-05-21 21:24:33 -0600

    updated session controller to the new passwordHash in user

--- backend/controllers/api/sessionController.js
+++ backend/controllers/api/sessionController.js
@@ -27,7 +27,7 @@ const login = async (req, res, next) => {
         return next(error);
     };
 
-    if (!bcrypt.compareSync(password, user.hashedPassword.toString())) {
+    if (!bcrypt.compareSync(password, user.passwordHash.toString())) {
         let error = new Error('Log In Failed');
         error.title = 'Login failed';
         error.message = 'Invalid Password';
