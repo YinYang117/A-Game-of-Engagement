338b603c4fdb6ff9404f257b19eae1ac1512d2d3
YinYang117 <slcryan2010@gmail.com>
2025-06-01 17:17:37 -0600

    structure to main, App, and Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      Hello World!
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+function Home() {
+  return (
+    <main className="home">
+      <section className="intro">
+        <h1>I Like Helping People</h1>
+        <p>I help people. That’s what I've always been about.</p>
+        {/* Continue building sections below */}
+      </section>
+    </main>
+  );
+}
+
+export default Home;
