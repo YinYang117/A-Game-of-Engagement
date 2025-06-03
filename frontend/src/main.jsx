b744e84c59258cd0f121ba56f3e665076c3a3d36
YinYang117 <slcryan2010@gmail.com>
2025-06-03 12:46:32 -0600

    font theme added, used in main.jsx, and home

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './css/globals/global.css';
-import './css/globals/globalTheme.css';
-let colorTheme = "theme-dark" // When user signin is used, pull from user data, state, or local storage
-document.body.className = colorTheme;
+import './css/globals/globalColorTheme.css';
+import './css/globals/globalFontTheme.css';
+
+let colorTheme = "theme-cool";
+let fontTheme = "font-github";
+// When user signin is used, pull from user data, state, or local storage
+document.body.classList.add(colorTheme, fontTheme);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
